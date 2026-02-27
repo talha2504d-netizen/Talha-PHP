d8cb6545be296176792be1f7f6ed303ba4f50a92
talha2504d-netizen <talha2504d@aptechsite.net>
2026-02-27 13:25:37 +0500

    Add files via upload

---
+++ Functions.php
@@ -0,0 +1,40 @@
+<?php
+
+$myvar = "abc123";
+$myvar1 = "My Name is Talha. This is talha speaking.";
+// echo strlen($myvar);
+
+// echo str_word_count($myvar1);
+// echo strrev($myvar);
+
+// echo strpos($myvar1, "Talha");
+
+
+// echo str_replace("Talha", "Ayesha", $myvar1);
+// echo ucwords($myvar1);
+
+
+$x = 2.9e532;
+// var_dump($x);
+
+$num = '345';
+// var_dump(is_numeric($num));
+// var_dump($num);
+
+
+$num = (float)$num;
+// var_dump($num);
+
+
+
+$myFloat = 1234.56;
+
+
+$myInteger = (int)$myFloat;
+// echo $myInteger;
+
+
+$myString = "34567";
+
+$myInteger2 = (int)$myString;
+// echo $myInteger2;
