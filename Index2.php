1e3754ec880b9b4892f5672407af3ff09e6ef3e2
talha2504d-netizen <talha2504d@aptechsite.net>
2026-03-06 13:24:41 +0500

    Add files via upload

--- Index2.php
+++ Index2.php
@@ -1,58 +1,111 @@
 <?php
-function test() {
-    define("Greeting", "Welcome to W3Schools.com!");
+// function test() {
+//     define("Greeting", "Welcome to W3Schools.com!");
+// }
+
+
+// test();
+// echo "<br>";
+// echo Greeting;
+
+
+// const myvar = "WATERMELON";
+// echo "<br>";
+// echo myvar;
+
+
+// echo "<br>";
+// echo __DIR__;
+
+
+// echo "\n";
+// echo __FILE__;
+
+
+
+// $x = 5;
+// $y = 10;
+
+// echo "<br>";
+// echo ($x <=> $y);
+
+// $x = 10;
+// $y = 10;
+
+// echo "<br>";
+// echo ($x <=> $y);
+
+
+// $x = 15;
+// $y = 10;
+
+// echo "<br>";
+// echo ($x <=> $y);
+
+
+// $fname = "Talha";
+// $lname = "Arham";
+
+// echo "<br>" . 145679023 . "<br>";
+
+// echo $fname . $lname . "<br>";
+
+// echo $fname . " " . $lname;
+
+// $age = 20;
+// $person = $age >= 18 ? "Adult" : "Teen";
+
+// echo "<br>" . $person;
+
+
+if (5 > 3) {
+    echo "<br>". "Have a good day!";
 }
 
+$a = 200;
+$b = 33;
+$c = 500;
 
-test();
-echo "<br>";
-echo Greeting;
+if ($a > $b and $a < $c) {
+    echo "<br>". "Both conditions are true";
+}
 
+date_default_timezone_set('Asia/Karachi');
 
-const myvar = "WATERMELON";
-echo "<br>";
-echo myvar;
+$t = date("H");
+echo $t;
 
+if ($t < "20") {
+    echo "<br>". "Have a good day!";
+} else {
+    echo "<br>". "Have a good night!";
+}
 
-echo "<br>";
-echo __DIR__;
+if ($t < "20") {
+    echo  "<br>". "Have a good day!";
+}
 
+else {
+    echo "<br>". "Have a good night!";
+}
 
-echo "\n";
-echo __FILE__;
+$age = 18;
 
+echo $age > 18 ? "<br>" . "You can get driving license" :"<br>" . "You are underage";
 
+$number = 45;
 
-$x = 5;
-$y = 10;
+echo $number > 33 ? "<br>" . "Number is greater" : "<br>" . "Number is lesser";
 
-echo "<br>";
-echo ($x <=> $y);
+$weight = 55;
 
-$x = 10;
-$y = 10;
+if ($age >= 25) {
+    if ($weight > 79) {
+    echo "<br>" . "You are eligible for swimming";
+    } else {
+        echo "<br>" . "You are not eligible for swimming";
+    }
+} else {
+    echo "<br>" . "You are not eligible for swimming";
+}
 
-echo "<br>";
-echo ($x <=> $y);
-
-
-$x = 15;
-$y = 10;
-
-echo "<br>";
-echo ($x <=> $y);
-
-
-$fname = "Talha";
-$lname = "Arham";
-
-echo "<br>" . 145679023 . "<br>";
-
-echo $fname . $lname . "<br>";
-
-echo $fname . " " . $lname;
-
-$age = 20;
-$person = $age >= 18 ? "Adult" : "Teen";
-
-echo "<br>" . $person;
